2e7a1223034a1c411c8c6dba92b396773023e477
FokinaMarharyta <FokinaMarharyta@outlook.com>
2025-12-16 23:05:56 +0100

    ud

---
+++ CreateBlog.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Export to PHP Array plugin for phpMyAdmin
+ * @version 5.2.3
+ */
+
+/**
+ * Database `CreateBlog`
+ */
+
+/* `CreateBlog`.`Register` */
+$Register = array(
+);
---
+++ register.php
@@ -0,0 +1,7 @@
+<?php
+ require_once "db.php";
+$login = $_POST['login'];
+$pass = $_POST['password'];
+$repeatpass = $_POST['repeatpass'];
+$email = $_POST['email'];
+
